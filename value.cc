a4fc88423ff575e17d061aaaca791d37cf68d23e
Jingguo Yao <yaojingguo@gmail.com>
2018-01-23 11:18:22 +0800

    update

---
+++ value.cc
@@ -0,0 +1,17 @@
+
+int prvalue(int n) {
+  return n;
+}
+
+int&  lvalue(int n) {
+  return n;
+}
+
+int&& xvalue(int n) {
+  return n;
+}
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
