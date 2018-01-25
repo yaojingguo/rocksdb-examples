1169cb60d9af49275959472611d5facf469e7b9e
Jingguo Yao <yaojingguo@gmail.com>
2018-01-25 14:26:52 +0800

    update

--- v.cc
+++ v.cc
@@ -11,8 +11,16 @@ void Plant::info() {
   cout << "I am a plant" << endl;
 }
 
+class Engineer {
+public:
+  virtual void greet() = 0;
+};
+
 int main(int argc, char const *argv[]) {
   Plant* p = new Plant();
   p->info();
+
+  // Engineer* e = new Engineer();
+
   return 0;
 }
