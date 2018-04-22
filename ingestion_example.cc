a50cb882ac8a6ecdf64e6250acab3477b869f05e
Jingguo Yao <yaojingguo@gmail.com>
2018-04-22 22:22:07 +0800

    update

---
+++ ingestion_example.cc
@@ -0,0 +1,17 @@
+#include <rocksdb/db.h>
+#include <rocksdb/options.h>
+#include <rocksdb/slice.h>
+#include <rocksdb/iterator.h>
+#include <rocksdb/utilities/transaction.h>
+#include <rocksdb/utilities/transaction_db.h>
+#include <rocksdb/slice_transform.h>
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char const *argv[]) {
+  
+  return 0;
+}
